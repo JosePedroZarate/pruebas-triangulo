5a80c66a6ae08e2a381894baa0325491e1b40e2b
Leonardo Martinez <jzaratespinosa2@gmail.com>
2021-11-15 13:12:13 -0600

    agregada la interfaz para calcular area #7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { CalculadoraComponent}  from './calculadora/calculadora.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LandingpageComponent
+  },
+  {
+    path: 'calculadora',
+    component: CalculadoraComponent
   }
 ];
 
